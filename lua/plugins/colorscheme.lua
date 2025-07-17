c93557deadc29354c31e09c8fde27e8a5ef50358
prasadj28 <prasadjadhav2895@gmail.com>
2025-07-17 19:02:45 -0400

    added themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,66 @@
 return {
   --Shortened Github Url
   --"Mofiqul/dracula.nvim"
-  "embark-theme/vim"
+  --"embark-theme/vim"
+  --"rebelot/kanagawa.nvim"
+  --"EdenEast/nightfox.nvim"
+  --"sainnhe/everforest"
+  --"olivercederborg/poimandres.nvim"
+  --"Mofiqul/adwaita.nvim"
+  --'Yazeed1s/minimal.nvim'
+  --"ficcdaf/ashen.nvim"
+  --"atmosuwiryo/vim-winteriscoming"
+  --"xiantang/darcula-dark.nvim"
+  --'ribru17/bamboo.nvim'
+  --"bluz71/vim-moonfly-colors"
+  --'shaunsingh/nord.nvim'
+  "motaz-shokry/gruvbox.nvim"
+  --"ntk148v/slack.nvim"
+  --"dark-orchid/neovim"
+  --'tiesen243/vercel.nvim'
+  --"kevinm6/kurayami.nvim"
+  --'b0o/lavi.nvim'
+  --"aliqyan-21/darkvoid.nvim"
+  --"datsfilipe/vesper.nvim"
+  --'kdheepak/monochrome.nvim'
+  --"kvrohit/rasmus.nvim"
+  --"zootedb0t/citruszest.nvim"
+  --"shaunsingh/moonlight.nvim"
+  --"AlexvZyl/nordic.nvim"
+  --"sainnhe/sonokai"
   ,
   lazy = false,
   priority = 1000,
   config = function()
   --  vim.cmd.colorscheme "dracula"
-  vim.cmd.colorscheme "embark"
+  --  vim.cmd.colorscheme "embark"
+  --  vim.cmd.colorscheme "kanagawa"
+  -- vim.cmd.colorscheme "carbonfox"
+  -- vim.cmd.colorscheme "everforest"
+  -- vim.cmd.colorscheme "poimanderes"
+  --  vim.cmd.colorscheme "adwaita"
+  -- vim.cmd.colorscheme "adwaita"
+  -- vim.cmd.colorscheme "ashen"
+  -- vim.cmd.colorscheme "WinterIsComing-dark-blue-color-no-italics-theme"
+  -- vim.cmd.colorscheme "darcula-dark"
+  -- vim.cmd.colorscheme "bamboo"
+  -- vim.cmd.colorscheme "moonfly"
+  -- vim.cmd.colorscheme "nord"
+   vim.cmd.colorscheme "gruvbox"
+  -- vim.cmd.colorscheme "slack"
+  -- vim.cmd.colorscheme "dark-orchid"
+  -- vim.cmd.colorscheme "vercel"
+  -- vim.cmd.colorscheme "kurayami"
+  -- vim.cmd.colorscheme "lavi"
+  -- vim.cmd.colorscheme "darkvoid"
+  -- vim.cmd.colorscheme "vesper"
+  -- vim.cmd.colorscheme "monochrome"
+  -- vim.cmd.colorscheme "rasmus"
+  -- vim.cmd.colorscheme "citruszest"
+  -- vim.cmd.colorscheme "moonlight"
+  -- vim.cmd.colorscheme "nordic"
+  -- vim.cmd.colorscheme "sonokai" --atlantis andromeda shusia maia espresso 
+
+
   end
 }
